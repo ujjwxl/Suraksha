00d7cc6d62f359fe49b3abbeff83e89eeb9b7837
ujjwxl <ujjwalk1408@gmail.com>
2024-06-02 14:33:14 +0530

    commit 10
    added map and modal for start tracking

--- lib/pages/main_home_page.dart
+++ lib/pages/main_home_page.dart
@@ -1,11 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:location/location.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class MainHomePage extends StatefulWidget {
-  const MainHomePage({Key? key}) : super(key: key);
+  const MainHomePage({super.key});
 
   @override
   State<MainHomePage> createState() => _MainHomePageState();
@@ -15,6 +17,10 @@ class _MainHomePageState extends State<MainHomePage> {
   late LatLng _currentLocation;
   bool _permissionGranted = false;
   bool _trackingStarted = false;
+
+  final TextEditingController _fromController = TextEditingController();
+  final TextEditingController _toController = TextEditingController();
+  final TextEditingController _licenseController = TextEditingController();
 
   @override
   void initState() {
@@ -70,45 +76,151 @@ class _MainHomePageState extends State<MainHomePage> {
       location.enableBackgroundMode(enable: true);
 
       trackingCollection.doc(userId).set({
+        'from': _fromController.text,
+        'to': _toController.text,
+        'license': _licenseController.text,
         'latitude': locationData.latitude,
         'longitude': locationData.longitude,
       });
     });
   }
 
+  void _showModalBottomSheet() {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (context) {
+        return Container(
+          padding: EdgeInsets.only(
+            bottom: MediaQuery.of(context).viewInsets.bottom,
+            top: 20.0,
+            left: 20.0,
+            right: 20.0,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Summary',
+                style: GoogleFonts.dmSans(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              TextField(
+                style: GoogleFonts.dmSans(fontSize: 16),
+                controller: _fromController,
+                decoration: InputDecoration(
+                  labelText: 'From',
+                  labelStyle: GoogleFonts.dmSans(fontSize: 14),
+                ),
+              ),
+              TextField(
+                style: GoogleFonts.dmSans(fontSize: 16),
+                controller: _toController,
+                decoration: InputDecoration(
+                  labelText: 'To',
+                  labelStyle: GoogleFonts.dmSans(fontSize: 14),
+                ),
+              ),
+              TextField(
+                style: GoogleFonts.dmSans(fontSize: 16),
+                controller: _licenseController,
+                decoration: InputDecoration(
+                  labelText: 'License Number',
+                  labelStyle: GoogleFonts.dmSans(fontSize: 14),
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _trackingStarted = true;
+                  });
+                  _startTracking();
+                  Navigator.pop(context); // Close the bottom sheet
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Color(0xFF1D0C2C)),
+                  padding: MaterialStateProperty.all(EdgeInsets.all(20.0)),
+                ),
+                child: Text('Start Tracking',
+                    style: GoogleFonts.dmSans(
+                      fontSize: 14,
+                      color: Colors.white,
+                    )),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        title: Text(
+          'Home',
+          style: GoogleFonts.dmSans(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
         actions: [
           IconButton(
             onPressed: () {
-              setState(() {
-                _trackingStarted = true;
-              });
-              _startTracking();
+              _showModalBottomSheet();
             },
-            icon: const Icon(Icons.play_arrow),
+            icon: const Icon(Icons.navigation_outlined),
           ),
         ],
       ),
       body: Center(
         child: _permissionGranted
-            ? Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text(
-                    'Current Location:',
+            ? FlutterMap(
+                options: MapOptions(
+                  initialCenter: _currentLocation,
+                  initialZoom: 17.2,
+                ),
+                children: [
+                  TileLayer(
+                    urlTemplate:
+                        // 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                        'https://maps.geoapify.com/v1/tile/osm-liberty/{z}/{x}/{y}.png?apiKey=790915664d99474e8ad53d13fbbd9484',
+                    userAgentPackageName: 'com.example.app',
                   ),
-                  Text(
-                    'Latitude: ${_currentLocation.latitude}',
-                  ),
-                  Text(
-                    'Longitude: ${_currentLocation.longitude}',
+                  MarkerLayer(
+                    markers: [
+                      Marker(
+                        point: _currentLocation,
+                        width: 250,
+                        height: 250,
+                        child: const Icon(
+                          Icons.location_on,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               )
+            // ? Column(
+            //     mainAxisAlignment: MainAxisAlignment.center,
+            //     children: <Widget>[
+            //       const Text(
+            //         'Current Location:',
+            //       ),
+            //       Text(
+            //         'Latitude: ${_currentLocation.latitude}',
+            //       ),
+            //       Text(
+            //         'Longitude: ${_currentLocation.longitude}',
+            //       ),
+            //     ],
+            //   )
             : const Text('Permission not granted'),
       ),
     );
